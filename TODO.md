14b3c28b1375ac221c978a07266f1948b1233504
ktwu01 <ktwu01@gmail.com>
2025-11-30 14:23:55 -0600

    Fix talkmap format and improve talks page formatting
    - Fixed org-locations.js array format to match official structure (description, lat, lng)
    - Capitalized title: 'Talks and Presentations'
    - Simplified map description text
    - Improved code formatting and indentation
    - Cleaned up TODO.md after task completion

--- TODO.md
+++
@@ -1,5 +0,0 @@
-- [x] add talkmap Based on instructions: `This map is generated from a Jupyter Notebook file in talkmap.ipynb, which mines the location fields in the .md files in _talks/.`
-
-- [x] https://ktwu01.github.io/talkmap This talkmap Not shown on the menu bar, but it shouldn't be shown on menu bar. Let we just merge the talk map into the talk. https://ktwu01.github.io/talks/
-
-- [ ] 
--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,7 +1,27 @@
 var addressPoints = [
-    [30.2672, -97.7431, "An open-source geospatial AI platform based on the AlphaEarth Foundation Model<br />Geoscience Hackathon '25; Austin, TX"],
-    [30.2672, -97.7431, "Noah-MP land surface model with plant hydraulics scheme (Noah-MP-PHS) Evaluation<br />Advancing Land Modeling Symposium; Austin, TX"],
-    [30.2672, -97.7431, "Noah-MP land surface model with plant hydraulics scheme (Noah-MP-PHS) Evaluation<br />JSG research symposium; Austin, TX"],
-    [40.0150, -105.2705, "Perturbations by the 2022 Hunga-Tonga Volcano Eruption in the MLT Region Investigated Using WACCM-X Simulation and Meteor Radar Observations<br />4th NASA DRIVE CGS Workshop; Boulder, CO"],
-    [37.7749, -122.4194, "Perturbations by the 2022 Hunga-Tonga Volcano Eruption in the MLT Region Investigated Using WACCM-X Simulation and Meteor Radar Observations<br />AGU 2023; San Francisco, CA"]
+    [
+        "An open-source geospatial AI platform based on the AlphaEarth Foundation Model<br />Geoscience Hackathon '25; Austin, TX",
+        30.2672,
+        -97.7431
+    ],
+    [
+        "Noah-MP land surface model with plant hydraulics scheme (Noah-MP-PHS) Evaluation<br />Advancing Land Modeling Symposium; Austin, TX",
+        30.2672,
+        -97.7431
+    ],
+    [
+        "Noah-MP land surface model with plant hydraulics scheme (Noah-MP-PHS) Evaluation<br />JSG research symposium; Austin, TX",
+        30.2672,
+        -97.7431
+    ],
+    [
+        "Perturbations by the 2022 Hunga-Tonga Volcano Eruption in the MLT Region Investigated Using WACCM-X Simulation and Meteor Radar Observations<br />4th NASA DRIVE CGS Workshop; Boulder, CO",
+        40.0150,
+        -105.2705
+    ],
+    [
+        "Perturbations by the 2022 Hunga-Tonga Volcano Eruption in the MLT Region Investigated Using WACCM-X Simulation and Meteor Radar Observations<br />AGU 2023; San Francisco, CA",
+        37.7749,
+        -122.4194
+    ]
 ];
